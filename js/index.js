fc6504e3df519cf3c017ba08e6f4ac7a17831238
Kiev Andres <kiev.mendoza@dharbor.com>
2023-03-08 10:50:18 -0400

    added social media buttons

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const socialMediaLinks = {
+  github: 'https://github.com/KievAndres',
+  facebook: 'https://www.facebook.com/profile.php?id=100090037513292',
+  twitter: 'https://twitter.com/KievAndresMRV',
+  linkedin: 'https://www.linkedin.com/in/kievandres/',
+  instagram: 'https://www.instagram.com/kievandres/'
+}
+const socialMediaButtons = document.getElementsByClassName('social-media-button');
+Array.from(socialMediaButtons).forEach(button => {
+  button.addEventListener('click', (event) => {
+    const link = socialMediaLinks[event.target.id];
+    window.open(link, '_blank')
+  })
+})
